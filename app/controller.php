1a95e8b748095698f4f938ab47239bdb7fafa97c
LeCongTai <lecongtai@yahoo.com.vn>
2019-09-22 09:55:26 +0700

    ss3

---
+++ app/controller.php
@@ -0,0 +1,31 @@
+<?php
+    class Controller{
+        public $layout = "";
+        public $view = "";
+        public function renderHTML($fileView,$data=NULL){
+            if($data != NULL){
+                extract($data);
+            }
+            // doc fileview
+            ob_start();
+            include $fileView;
+            $this->view = ob_get_contents();
+            ob_clean();
+            // ket thuc phien lam viec
+            // kiem tra xem co file layout k
+            if(file_exists($layout)){
+                include $this->layout;
+            }else{
+                echo $this->view;
+            }
+        }
+        public function Authentication(){
+            // day la ham xac thuc xem da dang nhap chua
+            if($_SESSION['account'] == NULL){
+                // neu khong co nghia la chua dang nhap
+                // t chuyen huong ve login
+                header("location:index.php?area=backend&controller=login");
+            }
+        }
+    }
+?>
---
+++ app/routing.php
@@ -0,0 +1,21 @@
+<?php
+    $area = "frontend";
+    $controller = "home";
+    $action = "index";
+    $area = isset($_GET['area']) ? $_GET['area'] : "$area";
+    $controller = isset($_GET['controller']) ? $_GET['controller'] : "$controller";
+    $action = isset($_GET['action']) ? $_GET['action'] : "$action";
+    $fileController = "controllers/$area/$controller/"."Controller.php";
+    if(file_exists($fileController)){
+        include $fileController;
+        // class
+        $className = $controller."Controller";
+        if(class_exists($className)){
+            // tao obj
+            $obj = new $className;
+            $obj->$action();
+        }else{
+            die(" class $className Khong ton tai");
+        }
+    }
+?>
