2794cfda4c77437b29b27409dfb2ab56bd4eb559
LeCongTai <lecongtai@yahoo.com.vn>
2019-09-22 09:53:20 +0700

    ss2

--- app/connection.php
+++ app/connection.php
@@ -1,3 +1,14 @@
 <?php
-
+    class Connection{
+        public function getInstance(){
+            global $host;
+            global $database;
+            global $user;
+            global $password;
+            // tao bien ket noi
+            $conn = new PDO("mysql:host=$host;dbname=$database","$user","$password");
+            $conn->exec("set names 'utf8'");
+            return $conn;
+        }
+    }
 ?>
